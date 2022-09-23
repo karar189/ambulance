91b109ea3858c63d2a4d38a10e991a94bfb7c121
swetakarar <kararsweta@gmail.com>
2022-09-23 23:18:33 +0530

    add: added map component {no style}

---
+++ src/Pages/Booking/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Map = () => {
+  return (
+    <>
+      <div className="map">
+        <h1>Map</h1>
+      </div>
+    </>
+  );
+};
+
+export default Map;
