1d85e867da60f52808dc30a0f9233380f9c49474
swetakarar <kararsweta@gmail.com>
2022-09-28 12:06:48 +0530

    fix: booking page alignment

--- src/Pages/Booking/Form/ConfirmBook/ConfirmBook.jsx
+++ src/Pages/Booking/Form/ConfirmBook/ConfirmBook.jsx
@@ -1,78 +1,70 @@
 import React from "react";
 import profile from "../../../../assets/driver-profile.svg";
 import "./style.css";
-import ambulance from "../../../../assets/ambulance-excel.svg"
-import phone from "../../../../assets/phone-logo.svg"
-import message from "../../../../assets/message-logo.svg"
-
+import ambulance from "../../../../assets/ambulance-excel.svg";
+import phone from "../../../../assets/phone-logo.svg";
+import message from "../../../../assets/message-logo.svg";
+import star from "../.././../../assets/stars.svg";
 
 const ConfirmBook = () => {
   return (
     <>
-        <div className = "confirm-details">
+      <div className="confirm-details">
+        <div className="driver-details">
+          <img className="profile" src={profile} alt="profile" />
 
-         <div className = "driver-details">
-              <img className="profile" src = {profile} alt="profile" />
-            
-                   <p className = "driver">Driver
-                   <br/><span><b>Rewant Chapri</b></span></p>
-                    <div className= "driver-ratings"> <p>3.5/5</p>
-                  </div>
-                   
-             </div>
+          <div className="driver">
+            <h3 className="driver-name">Driver</h3>
+            <p>Rewant Chapri</p>
+          </div>
 
-        <div className = "ambulance-details" >
-           <img className ="car" src = {ambulance} alt="car" />
-           <p className = "driver">Vehicle  No.<br/><span><b>WB-1826</b></span></p>
-           <div></div>  <div></div>  <div></div>
-           </div>
-          
-
-
-        <div className = "con-amb">Contact Ambulance</div>
-        <br/>
-        <div className = "contact-details">
-          <div className = "number">9883256806</div>
-           <img className="phone" src = {phone} alt = "phone-icon"/>
-           <img className="message" src = {message}alt = "message-icon"/>
+          <div className="driver-ratings">
+            {" "}
+            <img src={star} style={{ width: "90px" }} alt="" />
+            <p>3.5/5</p>
+          </div>
         </div>
-        <br/>
-        <div className = "pay">Payment method</div>
-        <br/>
-        <div className = "payment-details">
-           <div className = "cash">Cash</div>
-           <div className = "cash-amount">
-            Rs.1000</div>
+        <div className="car-details">
+          <img className="ambupic" src={ambulance} alt="profile" />
+
+          <div className="car">
+            <h3 className="car-name">Vehicle No.</h3>
+            <p>SF0459E</p>
+          </div>
+        </div>
+        {/* 
+        <div className="ambulance-details">
+          <img className="car" src={ambulance} alt="car" />
+          <p className="driver">
+            Vehicle No.
+            <br />
+            <span>
+              <b>WB-1826</b>
+            </span>
+          </p>
+          <div></div> <div></div> <div></div>
+        </div> */}
+
+        <div className="con-amb">Contact Ambulance</div>
+
+        <div className="contact-details">
+          <div className="number">+91 9883256806</div>
+          <img className="phone" src={phone} alt="phone-icon" />
+          <img className="message" src={message} alt="message-icon" />
+          <div className="number">Cancel</div>
+        </div>
+        <br />
+        <div className="pay">Payment method</div>
+
+        <div className="payment-details">
+          <div className="cash">Cash</div>
+          <div className="cash-amount">Rs.1000</div>
         </div>
 
         <hr></hr>
-
-
-
-        
-
-        
-
-        </div>
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
+      </div>
     </>
   );
 };
-
 
 export default ConfirmBook;
